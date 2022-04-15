bba8958129c8149274cee8fe0e70801e4495d0b1
shforeal <rivena14798@gmail.com>
2022-04-15 13:56:15 +0300

    added basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/scss/global.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <section className="container">Robot Shop</section>
       </header>
+      <main className="container">
+        <h1>Test</h1>
+      </main>
     </div>
   );
 }
