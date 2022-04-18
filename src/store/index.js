bba9e909a19174afdf56f1d9d352a4b5b4033a23
shforeal <rivena14798@gmail.com>
2022-04-18 20:38:06 +0300

    added redux store,

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import {robotReducer} from './reducers/robotReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  robotModule: robotReducer,
+  // robotModule: userReducer
+});
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+window.myStore = store;
