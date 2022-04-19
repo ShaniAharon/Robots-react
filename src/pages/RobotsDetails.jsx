46d4081f9f8ff908e41b13556fd37b045717340c
shforeal <rivena14798@gmail.com>
2022-04-19 17:44:03 +0300

    changed robot details to hooks

--- src/pages/RobotsDetails.jsx
+++ src/pages/RobotsDetails.jsx
@@ -1,52 +1,58 @@
-import {Component} from 'react';
+import {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
+import {useParams} from 'react-router-dom/cjs/react-router-dom.min';
 import {robotService} from '../services/robotService';
 
-export class RobotsDetails extends Component {
-  state = {
-    robot: null,
+export const RobotsDetails = (props) => {
+  const params = useParams();
+  const [robot, setRobot] = useState(null);
+
+  useEffect(() => {
+    loadRobot();
+  }, [params.id]);
+
+  // state = {
+  //   robot: null,
+  // };
+
+  // async componentDidMount() {
+  //   this.loadRobot();
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevProps.match.params.id !== this.props.match.params.id) {
+  //     this.loadRobot();
+  //   }
+  // }
+
+  const loadRobot = async () => {
+    const robot = await robotService.getById(this.props.match.params.id);
+    // this.setState({robot});
+    setRobot(robot);
   };
 
-  async componentDidMount() {
-    this.loadRobot();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.match.params.id !== this.props.match.params.id) {
-      this.loadRobot();
-    }
-  }
-
-  async loadRobot() {
-    const robot = await robotService.getById(this.props.match.params.id);
-    this.setState({robot});
-  }
-
-  onBack = () => {
-    this.props.history.push('/');
+  const onBack = () => {
+    props.history.push('/');
     // this.props.history.goBack()
   };
 
-  render() {
-    const {robot} = this.state;
-    if (!robot) return <div>Loading...</div>;
-    return (
-      <section className="robot-details">
-        <section>
-          <h3>Model: {robot.model}</h3>
-        </section>
-        <section>
-          <h3>Type: {robot.type}</h3>
-        </section>
-        <section>
-          <h3>battery status: {robot.batteryStatus}</h3>
-        </section>
-        <img src={`https://robohash.org/${robot._id}`} alt="" />
-        <section>
-          <button onClick={this.onBack}>Back</button>
-          <Link to="/robot/r2">Next Robot</Link>
-        </section>
+  if (!robot) return <div>Loading...</div>;
+  return (
+    <section className="robot-details">
+      <section>
+        <h3>Model: {robot.model}</h3>
       </section>
-    );
-  }
-}
+      <section>
+        <h3>Type: {robot.type}</h3>
+      </section>
+      <section>
+        <h3>battery status: {robot.batteryStatus}</h3>
+      </section>
+      <img src={`https://robohash.org/${robot._id}`} alt="" />
+      <section>
+        <button onClick={onBack}>Back</button>
+        <Link to="/robot/r2">Next Robot</Link>
+      </section>
+    </section>
+  );
+};
